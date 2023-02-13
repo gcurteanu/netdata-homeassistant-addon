251cd7af66f291846b0b0fdf7041e4454fcf6c8c
Gabriel Curteanu <gcurteanu@yahoo.com>
2023-02-13 19:37:08 +0200

    Update connect_to_netdata_cloud.sh

--- netdata/rootfs/usr/bin/connect_to_netdata_cloud.sh
+++ netdata/rootfs/usr/bin/connect_to_netdata_cloud.sh
@@ -7,4 +7,4 @@ NETDATA_CLOUD_URL=$(bashio::config 'claim_url')
 NETDATA_CLOUD_TOKEN=$(bashio::config 'claim_token')
 
 curl -s https://my-netdata.io/kickstart.sh > /tmp/netdata-kickstart.sh
-sh /tmp/netdata-kickstart.sh --claim-hostname ${NETDATA_HOSTNAME} --claim-token ${NETDATA_CLOUD_TOKEN} --claim-url ${NETDATA_CLOUD_URL}
+sh /tmp/netdata-kickstart.sh --claim-hostname ${NETDATA_HOSTNAME} --claim-token ${NETDATA_CLOUD_TOKEN} --claim-rooms ${NETDATA_CLOUD_ROOMS} --claim-url ${NETDATA_CLOUD_URL}--claim-url ${NETDATA_CLOUD_URL}
